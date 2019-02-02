e92671372123290b3c431e8c5b7f5d723417191c
Rohan Arora <rohanraarora@gmail.com>
2019-02-02 22:17:41 +0530

    Create components for header and content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
