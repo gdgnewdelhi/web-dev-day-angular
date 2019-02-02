766e464c15396c64400c43b4c2f86b818a5d799b
Rohan Arora <rohanraarora@gmail.com>
2019-02-02 22:50:05 +0530

    Add routerLink and redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     component: HomeComponent
   },
